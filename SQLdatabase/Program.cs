60685ec5de772297495ea5ff9ec20dcef3015bfa
Nick <nicklas.nick@live.dk>
2024-05-30 04:27:46 +0200

    Update Program.cs

--- SQLdatabase/Program.cs
+++ SQLdatabase/Program.cs
@@ -1,1 +1,46 @@
-﻿
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SQLdatabase;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddScoped<SqlDbRepo>((services) => {
+    var configuration = services.GetRequiredService<IConfiguration>();
+    var connectionString = configuration.GetConnectionString("DefaultConnection");
+    return new SqlDbRepo(connectionString);
+});
+builder.Services.AddControllers();
+
+// Configure CORS to allow specific origins
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder => builder.WithOrigins("http://localhost:4200")
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+});
+
+// Configure JSON options
+builder.Services.AddControllersWithViews()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.PropertyNamingPolicy = null;
+    });
+
+// Configure the HTTP request pipeline.
+var app = builder.Build();
+
+app.UseRouting();
+
+app.UseCors("AllowSpecificOrigin");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
